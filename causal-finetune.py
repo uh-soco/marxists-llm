805096e026bc2df4eb0c8943c956933e3c92a869
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2024-06-11 20:30:01 +0200

    Don't fail the whole model development if something works badly.

--- causal-finetune.py
+++ causal-finetune.py
@@ -16,60 +16,64 @@ models = models.split("\n")
 for model_name in models:
 
     print(f"Start {model_name}")
-    torch.manual_seed(0)
-    random.seed(0)
+    try:
+        torch.manual_seed(0)
+        random.seed(0)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.normalizer = tokenizer.normalizers.BertNormalizer( lowercase=False )
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.normalizer = tokenizer.normalizers.BertNormalizer( lowercase=False )
 
-    datasets = load_dataset("text", data_files={"train": './data/*.txt', "validation": './data/*.txt'})
+        datasets = load_dataset("text", data_files={"train": './data/*.txt', "validation": './data/*.txt'})
 
-    def tokenize_function(examples):
-        return tokenizer(examples["text"])
+        def tokenize_function(examples):
+            return tokenizer(examples["text"])
 
-    tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
+        tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
 
-    block_size = 2**7
+        block_size = 2**7
 
-    def group_texts(examples):
-        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        total_length = (total_length // block_size) * block_size
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        result["labels"] = result["input_ids"].copy()
-        return result
+        def group_texts(examples):
+            concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+            total_length = len(concatenated_examples[list(examples.keys())[0]])
+            total_length = (total_length // block_size) * block_size
+            result = {
+                k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+                for k, t in concatenated_examples.items()
+            }
+            result["labels"] = result["input_ids"].copy()
+            return result
 
-    lm_datasets = tokenized_datasets.map(
-        group_texts,
-        batched=True,
-        batch_size=1000,
-        num_proc=4,
-    )
-    
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+        lm_datasets = tokenized_datasets.map(
+            group_texts,
+            batched=True,
+            batch_size=1000,
+            num_proc=4,
+        )
+        
+        model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    training_args = TrainingArguments(
-        evaluation_strategy = "epoch",
-        learning_rate=2e-5,
-        num_train_epochs=10,
-        weight_decay=0.01,
-        output_dir= f"./models/{model_name.replace('/', '_')}-finetuned-causal-output",
-        logging_dir= f"./models/{model_name.replace('/', '_')}-finetuned-causal-logs",
-        logging_steps=10,
-    )
+        training_args = TrainingArguments(
+            evaluation_strategy = "epoch",
+            learning_rate=2e-5,
+            num_train_epochs=10,
+            weight_decay=0.01,
+            output_dir= f"./models/{model_name.replace('/', '_')}-finetuned-causal-output",
+            logging_dir= f"./models/{model_name.replace('/', '_')}-finetuned-causal-logs",
+            logging_steps=10,
+        )
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=lm_datasets["train"],
-        eval_dataset=lm_datasets["validation"],
-    )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=lm_datasets["train"],
+            eval_dataset=lm_datasets["validation"],
+        )
 
-    trainer.train()
-    print( f"Saving {model_name}" )
-    trainer.save_model(f"./models/{model_name.replace('/', '_')}-finetuned-causal")
-    tokenizer.save_pretrained(f"./models/{model_name.replace('/', '_')}-finetuned-causal")
+        trainer.train()
+        print( f"Saving {model_name}" )
+        trainer.save_model(f"./models/{model_name.replace('/', '_')}-finetuned-causal")
+        tokenizer.save_pretrained(f"./models/{model_name.replace('/', '_')}-finetuned-causal")
+    except Exception as e:
+        print( f"Failed {model_name}" )
+        print( e )
--- mask-finetune.py
+++ mask-finetune.py
@@ -21,67 +21,71 @@ models = models.split("\n")
 for model_name in models:
 
     print(f"Start {model_name}")
-    torch.manual_seed(0)
-    random.seed(0)
+    try:
+        torch.manual_seed(0)
+        random.seed(0)
 
-    datasets = load_dataset("text", data_files={"train": './data/*.txt', "validation": './data/*.txt'})
+        datasets = load_dataset("text", data_files={"train": './data/*.txt', "validation": './data/*.txt'})
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-    tokenizer.normalizer = tokenizers.normalizers.BertNormalizer( lowercase=False )
-  
-    def tokenize_function(examples):
-        return tokenizer(examples["text"])
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+        tokenizer.normalizer = tokenizers.normalizers.BertNormalizer( lowercase=False )
+    
+        def tokenize_function(examples):
+            return tokenizer(examples["text"])
 
-    tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
+        tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
 
-    block_size = 2*8
+        block_size = 2*8
 
-    def group_texts(examples):
-        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        total_length = (total_length // block_size) * block_size
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        result["labels"] = result["input_ids"].copy()
-        return result
+        def group_texts(examples):
+            concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+            total_length = len(concatenated_examples[list(examples.keys())[0]])
+            total_length = (total_length // block_size) * block_size
+            result = {
+                k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+                for k, t in concatenated_examples.items()
+            }
+            result["labels"] = result["input_ids"].copy()
+            return result
 
-    lm_datasets = tokenized_datasets.map(
-        group_texts,
-        batched=True,
-        batch_size=1000,
-        num_proc=4,
-    )
+        lm_datasets = tokenized_datasets.map(
+            group_texts,
+            batched=True,
+            batch_size=1000,
+            num_proc=4,
+        )
 
-    model = AutoModelForMaskedLM.from_pretrained( model_name )
+        model = AutoModelForMaskedLM.from_pretrained( model_name )
 
-    training_args = TrainingArguments(
-        evaluation_strategy = "epoch",
-        learning_rate=2e-5,
-        num_train_epochs=10,
-        weight_decay=0.01,
-        output_dir=  f"./models/{model_name.replace('/', '_')}-finetuned-masked-output",
-        logging_dir= f"./models/{model_name.replace('/', '_')}-finetuned-masked-logs",
-        logging_steps=10,
-    )
+        training_args = TrainingArguments(
+            evaluation_strategy = "epoch",
+            learning_rate=2e-5,
+            num_train_epochs=10,
+            weight_decay=0.01,
+            output_dir=  f"./models/{model_name.replace('/', '_')}-finetuned-masked-output",
+            logging_dir= f"./models/{model_name.replace('/', '_')}-finetuned-masked-logs",
+            logging_steps=10,
+        )
 
-    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+        data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=lm_datasets["train"],
-        eval_dataset=lm_datasets["validation"],
-        data_collator=data_collator,
-    )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=lm_datasets["train"],
+            eval_dataset=lm_datasets["validation"],
+            data_collator=data_collator,
+        )
 
-    trainer.train()
+        trainer.train()
 
-    #import math
-    #eval_results = trainer.evaluate()
-    #print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+        #import math
+        #eval_results = trainer.evaluate()
+        #print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
 
-    print( f"Saving {model_name}" )
-    trainer.save_model(f"./models/{model_name.replace('/', '_')}-finetuned-masked-model")
-    tokenizer.save_pretrained(f"./models/{model_name.replace('/', '_')}-finetuned-masked-model")
+        print( f"Saving {model_name}" )
+        trainer.save_model(f"./models/{model_name.replace('/', '_')}-finetuned-masked-model")
+        tokenizer.save_pretrained(f"./models/{model_name.replace('/', '_')}-finetuned-masked-model")
+    except Exception as e:
+        print( f"Failed {model_name}" )
+        print( e )
