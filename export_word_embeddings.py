1f303d1aa5872087c396faca3422775260ae4526
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2024-06-18 14:31:12 +0200

    Update to work with several models.

--- export_word_embeddings.py
+++ export_word_embeddings.py
@@ -5,39 +5,36 @@ import torch
 
 import csv
 
-torch.manual_seed(0)
-random.seed(0)
-
 from datasets import load_dataset
 from datasets import ClassLabel
 
 from transformers import AutoTokenizer
 from transformers import AutoModel
 
-model_name = "distilbert/distilbert-base-uncased"
-model_name = "./my_fine_tuned_model-masked/"
+for model_name in ["./models/microsoft_deberta-v3-base-finetuned-masked-model/", "microsoft/deberta-v3-base",]:
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+    model = AutoModel.from_pretrained(model_name)
 
-def get_word_embedding(word):
-    # Create input sentence with special tokens
-    input_text = f"[CLS] {word} [SEP]"
-    input_ids = tokenizer.encode(input_text, add_special_tokens=True)
-    input_ids = torch.tensor(input_ids).unsqueeze(0)  # Add batch dimension
 
-    # Get hidden states from BERT
-    with torch.no_grad():
-        outputs = model(input_ids)
-        hidden_states = outputs.last_hidden_state
+    def get_word_embedding(word):
+        # Create input sentence with special tokens
+        input_text = f"[CLS] {word} [SEP]"
+        input_ids = tokenizer.encode(input_text, add_special_tokens=True)
+        input_ids = torch.tensor(input_ids).unsqueeze(0)  # Add batch dimension
 
-    # Average hidden states for the word token
-    word_embedding = hidden_states.mean(dim=1).squeeze()
+        # Get hidden states from BERT
+        with torch.no_grad():
+            outputs = model(input_ids)
+            hidden_states = outputs.last_hidden_state
 
-    return word_embedding.numpy()
+        # Average hidden states for the word token
+        word_embedding = hidden_states.mean(dim=1).squeeze()
 
-csvout = csv.writer( open('model.csv', 'w', encoding='UTF8', newline='') )
+        return word_embedding.numpy()
 
-for word in tokenizer.vocab.keys():
-    embedding = get_word_embedding(word)
-    csvout.writerow( [ word ] + embedding.tolist() )
+    csvout = csv.writer( open(f"{model_name.replace('/', '_')}-word-embeddings.csv", 'w', encoding='UTF8', newline='') )
+
+    for word in tokenizer.vocab.keys():
+        embedding = get_word_embedding(word)
+        csvout.writerow( [ word ] + embedding.tolist() )
